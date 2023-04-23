89ff07293180b658c8d91b366e7e7a9ca3050c34
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-23 17:51:58 +0000

    new functions

--- _putchar.c
+++ _putchar.c
@@ -11,3 +11,18 @@ int _putchar(char c)
 	return (write(1 ,&c, 1));
 }
 
+/**
+ * _puts - prints string to stdout
+ * @s: string
+ * Return: returns number of chars printed
+ */
+int _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+	}
+	return (i);
+}
--- handle_int.c
+++ handle_int.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
-int handle_int(int num)
+int handle_int(va_list args, flags *fl)
 {
-	
+	int x = va_arg(args, int)
 }
+
+int counter(int num)
+{
+	unsigned int x =0, y;
+	
+	if (num < 0)
+		y = num * -1;
+	else
+		y = num;
+
+	while (y != 0)
+	{
+		y /= 10;
+		x++;
+	}
+	return (x);
+}
+
