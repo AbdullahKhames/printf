63029e105bc710740e9ae984e7383fcf4c4d84c7
Mado007 <mahmodeid2019@gmail.com>
2023-04-23 06:56:45 -0700

    Done Task 0

--- _printf.c
+++ _printf.c
@@ -8,49 +8,44 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				int c = va_arg(args, int);
-
-				write(1, &c, sizeof(c));
-				count++;
-			}
-
-			else if (*format == 's')
-			{
-			char *s = va_arg(args, char *);
-				while (*s != '\0')
-				{
-					write(1, s, sizeof(char));
-					s++;
-					count++;
-				}
-			}
-
-			else if (*format == '%')
-			{
-				write(1, "%", sizeof(char));
-				count++;
-			}
-		}
-
-		else
-		{
-			write(1, *format, sizeof(char));
-			count++;
-		}
-		format++;
-		}
-	va_end(args);
-	return (count);
+int count = 0;
+va_list args;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+{
+int c = va_arg(args, int);
+write(1, &c, 1);
+count++;
+}
+else if (*format == 's')
+{
+char *s = va_arg(args, char *);
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+write(1, s, len);
+count += len;
+}
+else if (*format == '%')
+{
+write(1, "%", 1);
+count++;
+}
+}
+else
+{
+write(1, format, 1);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
 }
