79d58041b68495af00b064ed3dc2a217c694454c
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-23 20:26:18 +0000

    new

--- main.h
+++ main.h
@@ -7,28 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Including my own function prototypes*/
-int _printf(const char *format, ...);
-int _putchar(char c);
-int _puts(char *s);
-int handle_string(va_list args, flags_type *f);
-int (*print_handler(char cha))(va_list, flags_type *f);
-int handle_int(va_list args, flags_type *f);
-int handle_unsigned(va_list args, flags_type *f);
-int handle_count(int x);
-int handle_hex(va_list args, flags_type *f);
-int handle_big_hex(va_list args, flags_type *f);
-int handle_binary(va_list args, flags_type *f);
-int handle_octal(va_list args, flags_type *f);
-int handle_char(va_list args, flags_type *f);
-int handle_rot13(va_list args, flags_type *f);
-int handle_rev(va_list args, flags_type *f);
-int handle_big_string(va_list args, flags_type *f);
-int handle_address(va_list args, flags_type *f);
-int handle_percent(va_list args, flags_type *f);
-int handle_number(int y);
-int get_flag(char cha, flags_type *f);
-char *converted_str(unsigned long int number, int base, int lowercase);
 /*structs*/
 
 /**
@@ -37,12 +15,12 @@ char *converted_str(unsigned long int number, int base, int lowercase);
  * @space: ' '
  * @hash: #
  */
-typedef struct flag
+typedef struct flags
 {
 	int plus;
 	int space;
 	int hash;
-} flags_type;
+} flags_t;
 
 /**
  * struct handleprint - struct
@@ -55,5 +33,27 @@ typedef struct handleprint
 	char c;
 	int (*f)(va_list args, flags_type *flags);
 } printer;
+/* Including my own function prototypes*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(char *s);
+int handle_string(va_list args, flags_t *f);
+int (*print_handler(char cha))(va_list, flags_t *f);
+int handle_int(va_list args, flags_type *f);
+int handle_unsigned(va_list args, flags_t *f);
+int handle_count(int x);
+int handle_hex(va_list args, flags_t *f);
+int handle_big_hex(va_list args, flags_t *f);
+int handle_binary(va_list args, flags_t *f);
+int handle_octal(va_list args, flags_t *f);
+int handle_char(va_list args, flags_t *f);
+int handle_rot13(va_list args, flags_t *f);
+int handle_rev(va_list args, flags_t *f);
+int handle_big_string(va_list args, flags_t *f);
+int handle_address(va_list args, flags_t *f);
+int handle_percent(va_list args, flags_t *f);
+int handle_number(int y);
+int get_flag(char cha, flags_type *f);
+char *converted_str(unsigned long int number, int base, int lowercase);
 
 #endif
---
+++ test.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+	int x = 5;
+	_printf("hello \n%p", &x);
+	return 0;
+}
