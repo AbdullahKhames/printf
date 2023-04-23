a83d1447a7a88264287a72ea42e8882b56a14451
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-23 19:22:42 +0000

    main.h

--- main.h
+++ main.h
@@ -6,8 +6,29 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "khamis.h"
 
+/* Including my own function prototypes*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _puts(char *s);
+int handle_string(va_list args, flags_type *f);
+int (*print_handler(char cha))(va_list, flags_type *f);
+int handle_int(va_list args, flags_type *f);
+int handle_unsigned(va_list args, flags_type *f);
+int handle_count(int x);
+int handle_hex(va_list args, flags_type *f);
+int handle_big_hex(va_list args, flags_type *f);
+int handle_binary(va_list args, flags_type *f);
+int handle_octal(va_list args, flags_type *f);
+int handle_char(va_list args, flags_type *f);
+int handle_rot13(va_list args, flags_type *f);
+int handle_rev(va_list args, flags_type *f);
+int handle_big_string(va_list args, flags_type *f);
+int handle_address(va_list args, flags_type *f);
+int handle_percent(va_list args, flags_type *f);
+int handle_number(int y);
+int get_flag(char cha, flags_type *f);
+char *converted_str(unsigned long int number, int base, int lowercase);
 /*structs*/
 
 /**
@@ -16,7 +37,6 @@
  * @space: ' '
  * @hash: #
  */
-
 typedef struct flag
 {
 	int plus;
@@ -24,19 +44,16 @@ typedef struct flag
 	int hash;
 } flags_type;
 
-/***/
+/**
+ * struct handleprint - struct
+ * @c: character
+ * @f: pointer to function
+ */
 
-typedef struct handle_print
+typedef struct handleprint
 {
 	char c;
 	int (*f)(va_list args, flags_type *flags);
 } printer;
 
-/* Including my own function prototypes*/
-int my_printf(const char *format, ...);
-int _printf(const char *format, ...);
-int _putchar(char c);
-int handle_string(char *s);
-int (*print_handler(char c))(va_list, flags_type *);
-int _puts(char *s);
 #endif
