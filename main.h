e46b3cd17f190481cb20d69734df93849e5a5f16
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-23 12:45:17 +0000

    added c standard library headers

--- main.h
+++ main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*includng C Standard Lib Headers*/
 #include <stdarg.h>
 #include <unistd.h>
 
+/* Including my own function prototypes*/
 int _printf(const char *format, ...);
 
 #endif
