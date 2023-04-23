5f6a23640da87e899669d150d38f27b03e88eb6a
Mado007 <mahmodeid2019@gmail.com>
2023-04-23 13:15:52 -0700

    our alpha

---
+++ handle_alpha.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * handle_string - loops through a string and prints
+ * every character
+ * @args: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int handle_string(va_list args, flags_type *f)
+{
+char *s = va_arg(args, char *);
+
+if (!s)
+s = "(null)";
+
+return (_puts(s));
+}
+
+/**
+ * handle_char - prints a character
+ * @args: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * if a flag is passed to _printf
+ * Return: number of char printed
+ */
+int handle_char(va_list args, flags_type *f)
+{
+_putchar(va_arg(args, int));
+(void)f;
+return (1);
+}
