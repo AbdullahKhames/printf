a197b80909124a08f4a1e65601829941615540fb
Mado007 <mahmodeid2019@gmail.com>
2023-04-23 13:22:40 -0700

    halndel alph

--- handle_alpha.c
+++ handle_alpha.c
@@ -8,7 +8,7 @@
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-int handle_string(va_list args, flags_type *f)
+int handle_string(va_list args, flags_t *f)
 {
 char *s = va_arg(args, char *);
 
@@ -25,7 +25,7 @@ return (_puts(s));
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-int handle_char(va_list args, flags_type *f)
+int handle_char(va_list args, flags_t *f)
 {
 _putchar(va_arg(args, int));
 (void)f;
